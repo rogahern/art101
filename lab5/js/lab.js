02a236f6482c83e689b6d6227b7b6adcb29b63c5
rogahern <rogahern@ucsc.edu>
2023-04-28 12:32:48 -0700

    js file

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,4 +1,4 @@
-// index.js - purpose and description here
+// index.js - lab 5
 // Author: Your Name
 // Date:
 
@@ -10,9 +10,26 @@
 function myFunction(param1, param2) {
   // some code here
   // return results;
+  //
 }
 
 function main() {
+
+var make = "Toyota";
+var model = "Corolla";
+var color = "Blue";
+var year = 2004;
+var ownIt = true;
+var currentYear = new Date().getFullYear();
+var age = currentYear - year;
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Own it: " + ownIt + "<br>");
+document.writeln("Age: " + age + "<br>");
+
   console.log("Main function started.");
   // the code that makes everything happen
 }
